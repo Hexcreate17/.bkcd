590b57b81208d2ff0ec66e6019a51dc77b8dedea
Hexcreate17 <sd1351571@gmail.com>
2026-04-21 15:13:23 +0530

    Initialize App.js for the Global News Aggregator App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Global News Aggregator</h1>
+      {/* Components will be added here */}
+    </div>
+  );
+};
+
+export default App;
